126a2d65b7b41a91b0d34516c0868c631055a6c6
mr_prave <vpraveen760@gmail.com>
2022-09-25 21:18:38 +0530

    array-1-FindTheMaximumAndMinimumElementInAnArray

---
+++ Array/1-FindTheMaximumAndMinimumElementInAnArray.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    pair<long long, long long> h;
+    long long mx=INT_MIN;
+    long long mi=INT_MAX;
+    for(int i=0;i<n;i++){
+        mx=max(mx,a[i]);
+        mi=min(mi,a[i]);
+    }
+    h.first = mi;
+    h.second = mx;
+    return h;
+}
