bf84436897f30b3c06a7fc826f252661c2f14974
mr_prave <vpraveen760@gmail.com>
2022-09-25 21:50:38 +0530

    Array-2-SortAnArrayOf0s1sAnd2s

--- Array/1-FindTheMaximumAndMinimumElementInAnArray.cpp
+++ Array/1-FindTheMaximumAndMinimumElementInAnArray.cpp
@@ -1,10 +1,10 @@
-
+//------Driver Code Start------>
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
+//-------Code Here------
 pair<long long, long long> getMinMax(long long a[], int n) ;
-
 int main() {
     int t;
     cin >> t;
@@ -12,15 +12,15 @@ int main() {
         int n;
         cin >> n;
         ll a[n];
-        for (int i = 0; i < n; i++) cin >> a[i];
-
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
         pair<ll, ll> pp = getMinMax(a, n);
-
         cout << pp.first << " " << pp.second << endl;
     }
     return 0;
 }
 
+//------Driver Code Start------>
 pair<long long, long long> getMinMax(long long a[], int n) {
     pair<long long, long long> h;
     long long mx=INT_MIN;
